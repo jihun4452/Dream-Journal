ee13ccaca5f4bac770f69d4c0e02de4b651a8ee6
박지훈 <jihun6548@naver.com>
2025-01-24 13:34:30 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/com/example/dreamjournal/DreamJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dreamjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamJournalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
