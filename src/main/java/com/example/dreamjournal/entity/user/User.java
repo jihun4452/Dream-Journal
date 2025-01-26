d6658d5d53a2c697d3ed19e2eea9b5b2a810b1ad
박지훈 <jihun6548@naver.com>
2025-01-26 21:18:02 +0900

    Feat: User추가, 설계이전

---
+++ src/main/java/com/example/dreamjournal/entity/user/User.java
@@ -0,0 +1,30 @@
+package com.example.dreamjournal.entity.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "User")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+    //설계 예정
+}
